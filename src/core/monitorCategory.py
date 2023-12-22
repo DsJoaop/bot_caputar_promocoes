183b51fb71b43791ffb76d308d3d8072e15576bd
João Paulo <joaopaulo.eu29@hotmail.com>
2023-12-22 07:07:50 -0300

    Fix

--- src/core/monitorCategory.py
+++ src/core/monitorCategory.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+from src.data_acess.extractPay import PichauAutomator
+
 
 class CategoryMonitor:
     def __init__(self, categoria, url, desconto_minimo, notificador, scraper):
@@ -10,18 +12,28 @@ class CategoryMonitor:
         self.notificador = notificador
         self.scraper = scraper
         self.produtos_precos = {}
+        self.produtos_notificados = set()
+        self.produtos_comprados = set()
 
     def enviar_notificacao(self, produto, previous_price, current_price, discount):
-        mensagem = (
-            f"🎉 Desconto detectado! 🎉\n\n\n"
-            f"🔗 Link: {produto.link}\n\n"
-            f"💰 Preço anterior: R${previous_price:.2f}\n\n"
-            f"💸 Novo preço: R${current_price:.2f}\n\n"
-            f"💲 Desconto: {discount:.2f}% OFF\n\n"
-            f"\n\n\n\n"
-        )
-        self.notificador.enviar_mensagem(mensagem)
-        print(f"Produto encontrado!!!\n\n {mensagem}")
+        if produto.link not in self.produtos_notificados:
+            mensagem = (
+                f"🎉 Desconto detectado! 🎉\n\n\n"
+                f"🔗 Link: {produto.link}\n\n"
+                f"💰 Preço anterior: R${previous_price:.2f}\n\n"
+                f"💸 Novo preço: R${current_price:.2f}\n\n"
+                f"💲 Desconto: {discount:.2f}% OFF\n\n"
+                f"\n\n\n\n"
+            )
+            self.notificador.enviar_mensagem(mensagem)
+            print(f"Produto encontrado!!!\n\n {mensagem}")
+            self.produtos_notificados.add(produto.link)
+
+    def realizar_compra(self, produto):
+        if produto.link not in self.produtos_comprados:
+            automator = PichauAutomator()
+            automator.run_automation(produto.link)
+            self.produtos_comprados.add(produto.link)
 
     def run(self):
         while True:
@@ -29,17 +41,14 @@ class CategoryMonitor:
             if produtos:
                 for produto in produtos:
                     previous_price = self.produtos_precos.get(produto.link)
-                    if previous_price is not None:
-                        current_price = produto.price
-                        discount = ((previous_price - current_price) / previous_price) * 100
+                    if previous_price is not None and produto.price < previous_price:
+                        discount = ((previous_price - produto.price) / previous_price) * 100
                         if discount >= self.desconto_minimo:
-                            self.enviar_notificacao(produto, previous_price, current_price, discount)
-
-                    self.produtos_precos[produto.link] = produto.price
-
+                            self.enviar_notificacao(produto, previous_price, produto.price, discount)
+                            self.realizar_compra(produto)
+                # Atualiza a lista de produtos inteira após o scraping
+                self.produtos_precos = {produto.link: produto.price for produto in produtos}
             else:
                 print(f"Nenhum produto encontrado para a categoria: {self.categoria}")
             print(f"Analisando {self.categoria}...")
-            # Aguarde um tempo aleatório entre 1 e 2 segundos antes de fazer o próximo scraping
-            time.sleep(random.uniform(1, 2))
-
+            time.sleep(random.uniform(60, 200))
--- src/data_acess/extractPay.py
+++ src/data_acess/extractPay.py
@@ -17,35 +17,48 @@ class PichauAutomator:
             os.path.join(assets_directory, '4_continuar_pagamento.png'),
             os.path.join(assets_directory, '5_boleto_bancario.png'),
             os.path.join(assets_directory, '6_continuar_revisao.png'),
-            os.path.join(assets_directory, '7_termos.png')
+            os.path.join(assets_directory, '7_termos.png'),
+            #os.path.join(assets_directory, '8_finalizar_agora.png')
         ]
         self.timeout_seconds = 10
 
     def path_exists(self, image_path):
         if not os.path.exists(image_path):
-            print(f"Arquivo não encontrado: {image_path}")
+            print(f"File '{image_path}' not found.\n")
             return False
         return True
+
+    def verify_image(self, image_path, y):
+        if image_path == self.img_paths[2]:
+            return y + 55
+        else:
+            return y
+
+    def wait(self, image_path):
+        if image_path in [self.img_paths[3], self.img_paths[5]]:
+            time.sleep(0.9)
 
     def search_on_screen(self, image_path):
         if not self.path_exists(image_path):
             return
 
         start_time = time.time()
+        self.wait(image_path)
 
         while time.time() - start_time < self.timeout_seconds:
             try:
                 x, y = pyautogui.locateCenterOnScreen(image_path, confidence=0.8)
+                y = self.verify_image(image_path, y)
                 pyautogui.click(x, y)
                 print("\n-----------------------------------------------------")
-                print(f"{image_path} encontrado.")
+                print(f"{image_path} found.")
                 print("-----------------------------------------------------\n")
 
                 return True
             except pyautogui.ImageNotFoundException:
                 print(f"Searching for {image_path}...")
 
-        print(f"Imagem {image_path} não encontrada após {self.timeout_seconds} segundos.")
+        print(f"Image path '{image_path}' not found on your screen after {self.timeout_seconds} seconds.\n")
         return False
 
     def run_automation(self, url):
@@ -54,18 +67,16 @@ class PichauAutomator:
         webbrowser.open(url)
 
         for img_path in self.img_paths:
-            if img_path == self.img_paths[5]:
-                time.sleep(0.3)
-
             if not self.search_on_screen(img_path):
+                print(
+                    f"The application was closed because it could not find the image '{img_path}' on your screen.\n")
                 break
 
         end_time = time.time()
         execution_time = end_time - start_time
-        print(f"Tempo de execução: {execution_time} segundos")
-
+        print(f"Execution time: {execution_time} seconds.")
 
 if __name__ == "__main__":
     automator = PichauAutomator()
     automator.run_automation(
-        "https://www.pichau.com.br/processador-amd-ryzen-5-7600-6-core-12-threads-3-8ghz-5-1ghz-turbo-cache-38mb-am5-100-100001015box")
+        "https://www.pichau.com.br/placa-mae-pichau-alphard-a520m-p-ddr4-socket-am4-chipset-amd-a520-pch-alpda520m-p")
