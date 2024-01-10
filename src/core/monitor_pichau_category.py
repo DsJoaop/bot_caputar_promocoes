574dc458def8e763e8f3c39affeb94cf84c149f6
João Paulo <joaopaulo.eu29@hotmail.com>
2024-01-10 07:09:04 -0300

    fix

--- src/core/monitor_pichau_category.py
+++ src/core/monitor_pichau_category.py
@@ -1,7 +1,6 @@
 import random
-import threading
 import time
-
+from copy import deepcopy
 from src.data_acess.buy_pichau import PichauAutomator
 from src.data_acess.scraper.extract_data_pichau import scraping_produtos_pichau, criar_produto_link_pichau
 
@@ -16,62 +15,47 @@ class CategoryMonitor:
         self.produtos_desejados = produtos_desejados
         self.automator = PichauAutomator()
 
-        # Primeira execução para definir produtos iniciais
         novos_produtos = scraping_produtos_pichau(self.url)
         for produto in novos_produtos:
             self.produtos[produto.link] = produto
 
     def verificar_desconto_produto_desejado(self, produto, preco_anterior, discount):
         if produto.link in self.produtos_desejados:
-            produto_anterior = self.produtos[produto.link]
-            if produto.price <= produto_anterior.max_price:
-                mensagem = (
-                    f'<a href="{produto.link_img}">&#8205;</a>'  # Link vazio para a imagem
-                    f"Compra desejada confirmada! 🔥🎁\n\n"
-                    f"<a href=\"{produto.link}\">🔗 {produto.nome}</a>\n\n"
-                    f"<b>🎉 Categoria: {produto.category}</b>\n"
-                    f"<b>🔥 Desconto: {discount:.2f}% OFF</b>\n\n"
-                    f"💰 <b>Preço anterior:</b> R${preco_anterior:.2f}\n"
-                    f"💸 <b>Novo preço:</b> R${produto.price:.2f}\n\n"
-                    f"🛒 <b>Abra a pichau e veja seu boleto!</b>\n"
+            produto_anterior = self.produtos.get(produto.link)
+            if produto_anterior and produto.price <= produto_anterior.max_price:
+                self.notificador.enviar_compra_confirmada(
+                    criar_produto_link_pichau(produto.link),
+                    preco_anterior,
+                    discount
                 )
                 self.automator.run_automation_boleto(produto.link)
-                notification_thread = threading.Thread(
-                    target=self.notificador.enviar_mensagem,
-                    args=(criar_produto_link_pichau(produto.link), mensagem)
-                )
-
-                notification_thread.start()
                 return
-        # Se não for um produto desejado ou não houver desconto, apenas envie a notificação
-        notification_thread = threading.Thread(
-            target=self.notificador.enviar_notificacao,
-            args=(criar_produto_link_pichau(produto.link), preco_anterior, discount)
+        self.notificador.enviar_notificacao(
+            criar_produto_link_pichau(produto.link), preco_anterior, discount
         )
-        notification_thread.start()
         self.produtos[produto.link] = produto
 
     def run(self):
         while True:
             start_time = time.time()
-
             novos_produtos = scraping_produtos_pichau(self.url)
 
+            produtos_copia = deepcopy(self.produtos)
             for novo_produto in novos_produtos:
                 link = novo_produto.link
-                if link in self.produtos:
-                    produto_anterior = self.produtos[link]
+                if link in produtos_copia:
+                    produto_anterior = produtos_copia[link]
                     discount = ((produto_anterior.price - novo_produto.price) / produto_anterior.price) * 100
                     if discount > 0:
-                        self.verificar_desconto_produto_desejado(criar_produto_link_pichau(novo_produto.link),
-                                                                 produto_anterior.price, discount)
+                        self.verificar_desconto_produto_desejado(
+                            criar_produto_link_pichau(novo_produto.link),
+                            produto_anterior.price,
+                            discount
+                        )
                 else:
-                    notification_thread = threading.Thread(
-                        target=self.notificador.enviar_alerta,
-                        kwargs={'product': criar_produto_link_pichau(novo_produto.link)}  # Passando o argumento como
-                        # um argumento nomeado
+                    self.notificador.enviar_alerta(
+                        criar_produto_link_pichau(novo_produto.link)
                     )
-                    notification_thread.start()
                     self.produtos[link] = novo_produto
 
             end_time = time.time()
--- src/telegram/telegram_notify.py
+++ src/telegram/telegram_notify.py
@@ -9,19 +9,12 @@ logger = logging.getLogger(__name__)
 
 class Notificacao:
     def __init__(self):
-        self.config = load_config()['telegram']
-        self.base_url = f"https://api.telegram.org/bot{self.config['bot_token']}/"
-        self.chat_id = self.config['chat_id']
+        self.config = load_config().get('telegram', {})
+        self.base_url = f"https://api.telegram.org/bot{self.config.get('bot_token', '')}/"
+        self.chat_id = self.config.get('chat_id', '')
 
     def enviar_mensagem(self, mensagem, reply_markup=None):
-        dados = {
-            'chat_id': self.chat_id,
-            'text': mensagem,
-            'parse_mode': 'HTML'
-        }
-        if reply_markup:
-            dados['reply_markup'] = reply_markup
-
+        dados = {'chat_id': self.chat_id, 'text': mensagem, 'parse_mode': 'HTML', 'reply_markup': reply_markup}
         url = f"{self.base_url}sendMessage"
         try:
             response = requests.post(url, json=dados)
@@ -33,44 +26,57 @@ class Notificacao:
             logger.exception(f"Erro ao enviar mensagem: {e}")
 
     def enviar_notificacao(self, product, previous_price, discount):
-        mensagem = (
-            f'<a href="{product.link_img}">&#8205;</a>'  # Link vazio para a imagem
-            f"<a href=\"{product.link}\">🔗 {product.nome}</a>\n\n"
-            f"<b>🎉 Categoria: {product.category}</b>\n"
-            f"<b>🔥 Desconto: {discount:.2f}% OFF</b>\n\n"
-            f"💰 <b>Preço anterior:</b> R${previous_price:.2f}\n"
-            f"💸 <b>Novo preço:</b> R${product.price:.2f}\n\n"
-            f"🛒 <b>Deseja comprar?!</b>\n"
-        )
+        mensagem = f'<a href="{product.link_img}">&#8205;</a>' \
+                   f'<a href="{product.link}">🔗 {product.nome}</a>\n\n' \
+                   f'<b>🎉 Categoria: {product.category}</b>\n' \
+                   f'<b>🔥 Desconto: {discount:.2f}% OFF</b>\n\n' \
+                   f'💰 <b>Preço anterior:</b> R${previous_price:.2f}\n' \
+                   f'💸 <b>Novo preço:</b> R${product.price:.2f}\n\n' \
+                   f'🛒 <b>Deseja comprar?!</b>\n'
 
         reply_markup = {
             'inline_keyboard': [
                 [{'text': 'Sim', 'callback_data': 'sim'}, {'text': 'Não', 'callback_data': 'nao'}]
             ]
         }
+
         self.enviar_mensagem(mensagem, reply_markup)
 
     def enviar_alerta(self, product):
         extrair_imagem_produto_pichau(product)
-        mensagem = (
-            f'<a href="{product.link_img}">&#8205;</a>'  # Link vazio para a imagem
-            f"<b>🎉 Novo {product.category}</b>\n\n"
-            f"<a href=\"{product.link}\">🔗 {product.nome}</a>\n\n"
-            f"💰 <b>Preço:</b> R${product.price:.2f}\n\n"
-            f"🛒 <b>Deseja comprar?!</b>\n"
-        )
+        mensagem = f'<a href="{product.link_img}">&#8205;</a>' \
+                   f'<b>🎉 Novo {product.category}</b>\n\n' \
+                   f'<a href="{product.link}">🔗 {product.nome}</a>\n\n' \
+                   f'💰 <b>Preço:</b> R${product.price:.2f}\n\n' \
+                   f'🛒 <b>Deseja comprar?!</b>\n'
 
         reply_markup = {
             'inline_keyboard': [
                 [{'text': 'Sim', 'callback_data': 'sim'}, {'text': 'Não', 'callback_data': 'nao'}]
             ]
         }
+
         self.enviar_mensagem(mensagem, reply_markup)
+
+    def enviar_compra_confirmada(self, product, preco_anterior, discount):
+        mensagem = (
+            f'<a href="{product.link_img}">&#8205;</a>'  # Link vazio para a imagem
+            f"Compra desejada confirmada! 🔥🎁\n\n"
+            f"<a href=\"{product.link}\">🔗 {product.nome}</a>\n\n"
+            f"<b>🎉 Categoria: {product.category}</b>\n"
+            f"<b>🔥 Desconto: {discount:.2f}% OFF</b>\n\n"
+            f"💰 <b>Preço anterior:</b> R${preco_anterior:.2f}\n"
+            f"💸 <b>Novo preço:</b> R${product.price:.2f}\n\n"
+            f"🛒 <b>Abra a pichau e veja seu boleto!</b>\n"
+        )
+
+        self.enviar_mensagem(criar_produto_link_pichau(product.link), mensagem)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     notify = Notificacao()
-    produto = criar_produto_link_pichau("https://www.pichau.com.br/cabo-extensor-premium-24p-pichau-ps100-rgb-240mm-branco"
-                                 "-pch-ps100-24prgbwht")
+    produto = criar_produto_link_pichau(
+        "https://www.pichau.com.br/cabo-extensor-premium-24p-pichau-ps100-rgb-240mm-branco"
+        "-pch-ps100-24prgbwht")
     notify.enviar_notificacao(produto, 50, 30)
